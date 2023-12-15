9800744fd79ca20679da6dbfecd529703f453ae2
Rithikeshh <rithikeshh@gmail.com>
2023-12-15 17:02:32 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -10,11 +10,15 @@ app.use(bodyParser.json());
 // Mock user data (Replace with actual user authentication logic)
 const users = [
   // Define user objects here
+  { id: 1, username: 'user1', password: 'password1' },
+  { id: 2, username: 'user2', password: 'password2' },
 ];
 
 // Mock product data (Replace with actual product retrieval logic)
 const products = [
   // Define product objects here
+  { id: 1, name: 'Product A', price: 10 },
+  { id: 2, name: 'Product B', price: 20 },
 ];
 
 // Authentication endpoint (Students should implement this)
@@ -24,6 +28,15 @@ app.post('/login', (req, res) => {
   // Example token generation:
   // const token = jwt.sign({ userId: user.id, username: user.username }, secretKey);
   // res.status(201).json({ token });
+  const {username, password} = req.body;
+  let user = users.find((u)=> u.username === username && u.password === password);
+  if (!user){
+    res.status(401).json({
+      message: "Authentication failed"
+    })
+  }
+  const token = jwt.sign({userId: user.id, username: user.name}, secretKey)
+  res.json({token})
 });
 
 // Product route (Students should implement this)
@@ -33,6 +46,13 @@ app.get('/product', (req, res) => {
   // If the token is valid, students can access product data and send it in the response
   // Example response:
   // res.status(200).json({ message: 'Product data', products: productData });
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedKey = jwt.verify(token, secretKey)
+    res.status(200).json({ message: 'Product data', products});
+  } catch (error) {
+    return res.status(401).json({"message": "Invalid token"})
+  }
 });
 
 module.exports = app;
